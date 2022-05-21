6a349bb42cbdef66b1f2a4f8adf4b95eba958465
vasshorin <)tSz%gBWz1>
2022-05-21 12:51:35 -0700

    Create login html, js, route

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 // ---------------
-// --- ROUTES  ---
+// --- CONSTS  ---
 // ---------------
-
 const express = require('express')
 const app = express()
 const bodyparser = require("body-parser");
 const https = require('https');
 const mongoose = require('mongoose');
+var session = require('express-session')
 app.set('view engine', 'ejs');
+
+// ---------------
+// --- APP USE---
+// ---------------
 app.use(express.static('./public'));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'ssshhhhh',
+    saveUninitialized: true,
+    resave: true
+}))
 
 const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => {
@@ -36,17 +46,66 @@ const timelineModel = mongoose.model("timelineevents", timelineSchema);
 
 // User Schema
 const userSchema = new mongoose.Schema({
-    name: String,
+    username: String,
+    firstName: String,
+    lastName: String,
     email: String,
     password: String,
+    time: String
 });
 const userModel = mongoose.model("users", userSchema);
 
+// ----------------
+// ----  USERS ----
+// ----------------
+
+// Signup
+app.put('/signup/create', function (req, res) {
+    console.log(req.body)
+    userModel.create({
+        username: req.body.username,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: req.body.password,
+        time: req.body.time
+    }, function (err, data) {
+        if (err) {
+            console.log('Error' + err)
+        } else {
+            console.log('Data' + data)
+        }
+        res.send("New user created!")
+    })
+})
+
+// Login
+app.post('/login/authentication', function (req, res, next) {
+    userModel.find({}, function (err, users) {
+        if (err) {
+            console.log('Error' + err)
+        } else {
+            console.log('Data' + users)
+        }
+
+        user=users.filter((userobj)=>{
+            return userobj.email == req.body.email
+        })
+        if (user[0].password==req.body.password){
+            req.session.authenticated = true
+            req.session.email = req.body.email
+            req.session.userId = user[0]._id
+            req.session.userobj = user[0]
+            // LoggedInUserID = req.session.userId
+            res.send("Successful Login!" + req.session.userobj + "user id: " + req.session.userId)
+        }
+
+    })
+})
 
 // ----------------
 // --- TIMELINE ---
 // ----------------
-
 
 app.use(bodyparser.urlencoded({
     parameterLimit: 100000,
